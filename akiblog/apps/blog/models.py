899d8f08904894a1444ed49058b305108777a5a9
Alexey <aki@it-technologies.us>
2020-03-24 22:58:09 +0300

    added image and slug fields to Articles model

--- akiblog/apps/blog/models.py
+++ akiblog/apps/blog/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 from django.utils import timezone
 import datetime
+
+
+def slugify_fx(content):
+    return content.replace('_', '-').lower()
 
 class Article(models.Model):
     article_title = models.CharField('Название статьи', max_length=200)
     article_text = models.TextField('Текст статьи')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     views = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='media/images/', blank=True, null=True)
+    # slug = models.SlugField(blank=True, null=True, unique=True)
+    slug = AutoSlugField(populate_from='article_title', slugify_function=slugify_fx)
     #photos
     #videos
+    #TODO При загрузке фото на лету создавать папку с ID статьи и грузить туда изображения
 
     def __str__(self):
         return self.article_title
